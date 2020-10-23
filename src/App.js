7857fe5b5a89695dfd9a84b4e9b816e5b2f30a1c
Alexis <alecabj12@gmail.com>
2020-10-23 12:22:05 -0500

    contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    value: 0,
+  };
+
+  counter = () => {
+    this.setState({
+      value: this.state.value + 1,
+    });
+  };
+
   render() {
+    
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button onClick={this.counter} id="inc">Incrementa</button>
       </div>
     );
   }
